b8e30ceb408ca3cf83ae1ef9d03c11dd14bc3e55
Arohi1Jambenal <103898925+Arohi1Jambenal@users.noreply.github.com>
2023-01-07 00:02:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+
+import cv2
+
+img = "toddler.jpg"
+
+
+    
+image = cv2.imread('toddler.jpg')
+
+gray_img = cv2.cvtColor(image , cv2.COLOR_BGR2GRAY)
+invert = cv2.bitwise_not(gray_img)
+
+blur = cv2.GaussianBlur(invert , (25,25), 0)
+
+inverted_blur = cv2.bitwise_not(blur)
+
+sketch = cv2.divide(gray_img , blur , inverted_blur)
+
+cv2.imwrite('sketch.png', sketch)
